ee2db3ce70bfdbe0607fc4a7a542606f592529ea
damianhaq <damianhaq@gmail.com>
2024-03-25 08:19:44 +0100

    load tilemap from Tiled and just render it

--- app.js
+++ app.js
@@ -7,16 +7,26 @@ var config = {
     create: create,
     update: update,
   },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-  this.load.image("spritesheet", "BigSpritev7.png");
+  this.load.image("tiles", "BigSpritev7.png");
+  this.load.tilemapTiledJSON("map", "map.json");
 }
 
 function create() {
-  this.add.image(0, 0, "spritesheet");
+  const map = this.make.tilemap({ key: "map", tileWidth: 16, tileHeight: 16 });
+  const tileset = map.addTilesetImage("tiles1", "tiles");
+  const layer = map.createLayer("topLayer0", tileset, 0, 0);
 }
 
 function update() {}
